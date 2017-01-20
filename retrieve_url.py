0405ab0a568d040e90cb558f681c35067d42487a
Rajat Singh <therevolutionizt@gmail.com>
2017-01-20 11:15:50 +0530

    keywords extraction

--- retrieve_url.py
+++ retrieve_url.py
@@ -1,6 +1,9 @@
 from google import google
 from constants import IBM_WATSON_API_KEY
+from keywords.rake import *
 def retrieve_keywords(article):
+    imp_keywords = extract_keywords(article)
+    return imp_keywords
     
     
 def search(query)
