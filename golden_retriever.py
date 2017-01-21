879cc131c1702565f699465732fd0cdf98cb2bea
Akirato <nurendra.choudhary@research.iiit.ac.in>
2017-01-21 15:03:16 +0530

    Useless stuff removed

--- golden_retriever.py
+++ golden_retriever.py
@@ -1,10 +1,4 @@
 from google import google
-from constants import IBM_WATSON_API_KEY
-from keywords.rake import *
-def retrieve_keywords(article):
-    imp_keywords = extract_keywords(article)
-    return imp_keywords
-    
     
 def search(query)
     return google.search(' '.join(query))
--- retrieve_url.py
+++
@@ -1,6 +0,0 @@
-from google import google
-from constants import IBM_WATSON_API_KEY
-
-def search(query)
-    return google.search(' '.join(query))
-
