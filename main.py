8ca5db3ace777e2f4cd60861ddca28eb4583807c
Rajat Singh <therevolutionizt@gmail.com>
2017-01-21 14:46:19 +0530

    initiating production

--- main.py
+++ main.py
@@ -2,5 +2,23 @@
 # Research Center : LTRC, IIIT Hyderabad
 __author__ = ["Akirato","revsi"]
 
-def main(content):
-    pass
+from keywy import rake
+debug = True
+def main():
+    with open('input.txt', 'r') as myfile:
+        text=myfile.read()
+    print "============================CREDO============================"
+    print "Input Text : "+text
+    print "===========================Results===========================\n\n"
+
+    #keywords extraction
+    keywords = rake.extract_keywords(text)
+    if debug:
+        print "===========================Keywords===========================\n\n"
+        print keywords
+
+    
+
+    
+
+main()
---
+++ retrieve_url.py
@@ -0,0 +1,6 @@
+from google import google
+from constants import IBM_WATSON_API_KEY
+
+def search(query)
+    return google.search(' '.join(query))
+
